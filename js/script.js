b2536165994ca7c262b18d994ccf88ddf3288784
DilshanIndunil <163544478+DilshanIndunil@users.noreply.github.com>
2024-06-15 11:11:33 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var navLinks = document.getElementById("navLinks");
+function showMenu() {
+  navLinks.style.right = "0";
+}
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
+
+var swiper = new Swiper(".swiper", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
